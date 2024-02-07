05b9cef31728c8d7a140ca32b895d4f1421e2933
Jake Neal <hello@jakeneal.co.uk>
2024-02-07 21:30:20 +0000

    :white_check_mark: added tests for productcard

--- src/app/components/ProductCard/ProductCard.spec.jsx
+++ src/app/components/ProductCard/ProductCard.spec.jsx
@@ -3,10 +3,47 @@ import { describe, test, expect } from 'vitest';
 import { ProductCard } from './ProductCard';
 
 describe('ProductCard', () => {
-    test('renders', () => {
-        const { getByTestId } = render(<ProductCard />);
-        const heading = getByTestId('heading');
+    let card;
 
-        expect(heading.textContent).toBe('Item');
+    beforeEach(() => {
+        card = render(
+            <ProductCard
+                type={'T-shirt'}
+                brand={'Burberry'}
+                description={'A T-shirt'}
+                imageUrl={'https://via.placeholder.com/100x100'}
+                colour={'White'}
+                price={{
+                    retail: '£100',
+                    rental: {
+                        1: '£10',
+                    },
+                }}
+                minimumRentalPeriod={'1 days'}
+                tags={['oversized']}
+            />,
+        );
+    });
+
+    test('renders the image', () => {
+        const { getByTestId } = card;
+        const image = getByTestId('image');
+
+        expect(image).toHaveAttribute('src', 'https://via.placeholder.com/100x100');
+        expect(image).toHaveAttribute('alt', 'A T-shirt');
+    });
+
+    test('renders the brand', () => {
+        const { getByTestId } = card;
+        const brand = getByTestId('brand');
+
+        expect(brand.textContent).toBe('Burberry');
+    });
+
+    test('renders the description', () => {
+        const { getByTestId } = card;
+        const description = getByTestId('description');
+
+        expect(description.textContent).toBe('A T-shirt');
     });
 });
