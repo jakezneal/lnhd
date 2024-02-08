112f7ee577c856d635465a3d899891ad9f2b006c
Jake Neal <hello@jakeneal.co.uk>
2024-02-08 00:24:32 +0000

    :bento: updated css import paths

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { unstable_noStore as noStore } from 'next/cache';
 import Products from './_components/Products';
 import { serverClient } from '~/trpc/serverClient';
 
-import styles from './index.module.css';
+import styles from './css/home.module.css';
 
 export default async function Home() {
     noStore();
